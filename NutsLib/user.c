025f2b19cf62b383b004251c132527239f74f8f5
HUANsic <wu-zh20@mails.tsinghua.edu.cn>
2025-11-13 12:56:49 +0800

    working encrypt

--- NutsLib/user.c
+++ NutsLib/user.c
@@ -32,10 +32,64 @@ NutStatus_e Echo(uint8_t *received_data_ptr, uint32_t received_data_length, uint
 	return NUT_OK;
 }
 
+NutStatus_e Encrypt(uint8_t *received_data_ptr, uint32_t received_data_length, uint8_t *result_buffer_ptr, uint32_t *result_length,
+		uint32_t result_buffer_MAX_size) {
+	uint32_t i;
+	uint8_t tempu8, text;
+	uint8_t key = received_data_ptr[0];
+
+	// trigger set
+	HAL_Delay(100);
+	Nut_Trigger_Set();
+	// delay approximately 10us
+	for(i = 0; i < 10; i++){
+		__asm__("nop");
+		__asm__("nop");
+		__asm__("nop");
+		__asm__("nop");
+		__asm__("nop");
+
+		__asm__("nop");
+		__asm__("nop");
+		__asm__("nop");
+		__asm__("nop");
+		__asm__("nop");
+
+		__asm__("nop");
+		__asm__("nop");
+		__asm__("nop");
+		__asm__("nop");
+		__asm__("nop");
+	}
+
+	// encrypt
+	text = received_data_ptr[1];
+	for (i = 2; i < received_data_length; i++) {
+		if((0x01 & text) == (0x01 & key)) {
+			tempu8 = (0x80 & text);
+			text <<= 1;
+			text |= tempu8 ? 0x01 : 0x00;
+		} else {
+			text ^= key;
+		}
+	}
+
+	// trigger clear
+	HAL_Delay(10);
+	Nut_Trigger_Clear();
+
+	// return
+	result_buffer_ptr[0] = text;
+	*result_length = 1;
+	return NUT_OK;
+}
+
 /* User command */
 // @formatter:off
 NutAction_t command_list[] = {
 		{.command=0x0001, .function=Echo},
+		// example
+		{.command=0x0002, .function=Encrypt},
 };
 // @formatter:on
 uint16_t command_count = sizeof(command_list) / sizeof(command_list[0]);
